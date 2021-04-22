0d6b6461ebff704f7d4ba95146bb4b24697b039a
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-22 15:42:32 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,85 @@
 import React from "react";
 // import EmployeeCard from "./components/EmployeeCard.js";
-import SearchForm from "./components/SearchForm";
+// import SearchForm from "./components/SearchForm";
 import API from "./utils/API";
 
 class App extends React.Component {
-  state = {
-    employees: [],
-  };
+    state = {
+        employees: [],
+    };
 
-  componentDidMount() {
-    this.getEmployees();
-  }
+    componentDidMount() {
+        this.getEmployees();
+    }
 
-  getEmployees = async () => {
-    const { data } = await API.getUsers();
-    const employees = data.results.map(item => ({
-        image:item.picture.thumbnail,
-        name:`${item.name.first} ${item.name.first}`,
-        email:item.email, 
-        phone:item.phone,
-        dob:item.dob,
+    getEmployees = async () => {
+        const { data } = await API.getUsers();
+        const employees = data.results.map(item => ({
+            image: item.picture.thumbnail,
+            name: `${item.name.first} ${item.name.first}`,
+            email: item.email,
+            phone: item.cell,
+            dob: item.dob.date,
 
-    }))
-    this.setState({ employees });
-  };
+        }))
+        this.setState({ employees });
+    };
+    filterEmployees = (employee) => {
+        if (employee.name.includes(this.state.search)) {
+            return true;
+        }
+        return false;
+    };
 
-  handleInputChange = event => {
-    const name = event.target.name;
-    const value = event.target.value;
-    this.setState({
-      [name]: value
-    });
-  };
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    };
 
-   // When the form is submitted, search the randomuser API for `this.state.search`
-   handleFormSubmit = event => {
-    event.preventDefault();
-    this.getEmployees(this.state.search);
-  };
+    // When the form is submitted, search the randomuser API for `this.state.search`
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getEmployees(this.state.search);
+    };
 
-  render() {
-    console.log(this.state);
-    return 
-        // <div>
+    render() {
+        // console.log(this.state);
+        const { employees } = this.state;
+        return (
+            <table>
+                <thead>
+                    <th>image</th>
+                    <th>name</th>
+                    <th>email</th>
+                    <th>phone</th>
+                    <th>DOB</th>
+                </thead>
+                <tbody>
+                {employees.length === 0 ? (<td> No employees!</td>
+                ) : (
+                    employees.filter(this.filterEmployees).map((employee =>
+                    (<tr>
+                        <td><img src={employee.image} alt={''}/></td>
+                        <td>{employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.phone}</td>
+                        <td>{employee.dob}</td>
+                    </tr>
+                    ))))}
+                    </tbody>
+
+            </table>
+        );
+
+    }
+}
+
+export default App;
+
+// <div>
         //   <SearchForm
         //     search={this.state.search}
         //     handleFormSubmit={this.handleFormSubmit}
@@ -50,17 +87,3 @@ class App extends React.Component {
         //   />
         //   <EmployeeCard results={this.state.results} />
         // </div>
-        <table>
-        <thead>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        </thead>
-        </table>
-      
-    }
-  }
-
-export default App;
