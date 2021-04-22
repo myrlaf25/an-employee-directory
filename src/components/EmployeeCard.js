84e833f831fa8461e999d00afc2081561ea7310d
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-21 21:16:40 -0700

    changes to employeecard

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -5,15 +5,15 @@ function EmployeeCard(props) {
     return (
       <>
         <div className="card-group"/>
-        {props.map((item)=>(     
+        {props.results.map((result)=>(     
             <div className="card"/>
-                <img key={item.id} className="card-img-top" src={item.image} alt={item.name}/>
+                <img key={result.id} className="card-img-top" src={result.images.original.url} alt={result.title}/>
                 ))}
                     <div className="card-body">
-                        <p className="card-text">{props.name}</p>
-                        <p className="card-text">{props.phone}:</p>
-                        <p className="card-text">{props.email}</p>
-                        <p className="card-text">{props.dob}</p>
+                        <p className="card-text">{result.name}</p>
+                        <p className="card-text">{result.phone}:</p>
+                        <p className="card-text">{result.email}</p>
+                        <p className="card-text">{result.dob}</p>
                     </div>
           </>
     );
