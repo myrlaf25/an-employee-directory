900aac3525a433baeb7642a676c384779e089764
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-23 10:52:15 -0700

    update

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -5,19 +5,24 @@ import "../styles/EmployeeCard.css";
 function EmployeeCard({ name, image, phone, email, dob }) {
     return (
         <>
-        
-            <div key={name}>
-            <div className="card-body">
-            <img src={image} alt={name} />
-                        <p className="card-text">Name: {name}</p>
-                        <p className="card-text">Phone: {phone}:</p>
+
+            <div key={name} className="card-group">
+                <div className="card">
+                    <div className="card-body">
+                        <img className="card-img-top" src={image} alt={name} />
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">Phone: {phone}</p>
                         <p className="card-text">Email: {email}</p>
                         <p className="card-text">DOB: {dob}</p>
                     </div>
+                    
+                </div>
             </div>
-          
-            </>
-     )}
+
+
+        </>
+    )
+}
 
 
 export default EmployeeCard;
